e4a94a95a8ba0a2054226f6286ab60556ebf1e99
CubeTures <91618801+CubeTures@users.noreply.github.com>
2024-09-17 20:44:28 -0500

    Dummy Enemy

---
+++ MusicMurder/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    const string playerTag = "Player";
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(playerTag))
+        {
+            DestroyEnemy();
+        }
+    }
+
+    void DestroyEnemy()
+    {
+        Destroy(this);
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Enemy Died");
+    }
+}
