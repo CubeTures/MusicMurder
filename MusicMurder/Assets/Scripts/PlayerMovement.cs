943add853f19ceb5a6e50747ae98de553cbeab51
MitosisMan <99696985+MitosisMan@users.noreply.github.com>
2024-09-18 12:05:42 -0500

    Working movement

--- MusicMurder/Assets/Scripts/PlayerMovement.cs
+++ MusicMurder/Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,9 @@ using static System.Math;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-    private bool isMoving;
+    [SerializeField] private bool isMoving;
     private Vector2 currentTile, nextTile;
+    int speed;
     static float t = 0.0f;
 
     // Start is called before the first frame update
@@ -18,13 +19,26 @@ public class PlayerMovement : MonoBehaviour
         body = GetComponent<Rigidbody2D>();
         currentTile = body.position;
         isMoving = false;
+        speed = 100;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = 0;
+        float horizontal = 0;
+        if(Input.GetKeyDown(KeyCode.W)){
+            vertical = 1;
+        }
+        if(Input.GetKeyDown(KeyCode.S)){
+            vertical = -1;
+        }
+        if(Input.GetKeyDown(KeyCode.A)){
+            horizontal = -1;
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            horizontal = 1;
+        }
         if((Math.Abs(vertical) > .2f || Math.Abs(horizontal) > .2f) && !isMoving){
             isMoving = true;
             currentTile.x = body.position.x;
@@ -38,7 +52,14 @@ public class PlayerMovement : MonoBehaviour
             }
         }
 
-        body.position = new Vector2(Mathf.Lerp(currentTile.x, nextTile.x, t), Mathf.Lerp(currentTile.y, nextTile.y, t));
-        t += 0.5f * Time.deltaTime;
+        if(isMoving){
+            body.position = new Vector2(Mathf.Lerp(currentTile.x, nextTile.x, t), Mathf.Lerp(currentTile.y, nextTile.y, t));
+            t += 0.1f * Time.deltaTime * speed;
+        }
+
+        if(body.position == nextTile){
+            isMoving = false;
+            t = 0.0f;
+        }
     }
 }
