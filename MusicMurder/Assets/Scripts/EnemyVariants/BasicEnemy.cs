8bd4c3baf095c761b0fbcb014dae5948a71c5aaa
Owen Shadburne <91618801+CubeTures@users.noreply.github.com>
2024-11-17 10:17:14 -0600

    Less invasive overlay

--- MusicMurder/Assets/Scripts/EnemyVariants/BasicEnemy.cs
+++ MusicMurder/Assets/Scripts/EnemyVariants/BasicEnemy.cs
@@ -14,10 +14,3 @@ public class BasicEnemy : Enemy
         SetDirectionFromPathfinding();
     }
 }
-
-/** Area - fix to not move until attack is done
- * 
- * CheckerboardEnemy (Area attack in cheakerboard pattern, long wind up but hard to avoid)
- * BubbleEnemy (attack around self)
- * TriangleAreaEnemy (attack in triangle, but not as often)
- */
