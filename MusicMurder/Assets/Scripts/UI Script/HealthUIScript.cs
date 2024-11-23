9fb1cbcea91724a9c19617c4011e866d2314fb56
TurtlePancake <alecpt2004@gmail.com>
2024-11-22 19:42:33 -0600

    Health Updated, NOT THE MOST OPTIMIZED WAY BUT IT IS OKAY
    VERY UNOPTIMIZED WAY OF DOING IT

--- MusicMurder/Assets/Scripts/UI Script/HealthUIScript.cs
+++ MusicMurder/Assets/Scripts/UI Script/HealthUIScript.cs
@@ -6,13 +6,20 @@ using static UnityEditor.ShaderData;
 
 public class HealthUIScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public GameObject Full;
-    public GameObject Half;
-    public GameObject Low;
-
     
+    //IGNORE THE NAMING SCEME
+    public GameObject Full;
+    public GameObject Five;
+    public GameObject Half;
+    public GameObject three;
+    public GameObject Low;
+    public GameObject One;
 
+
+    private void Start()
+    {
+        UpdateHealth();
+    }
 
     private void Awake()
     {
@@ -25,28 +32,67 @@ public class HealthUIScript : MonoBehaviour
     }
 
 
-    public void UpdateHealth(int health)
+    public void UpdateHealth(int health = 6)
     {
+        //Not a great solution but gets the job done
+        if (health >= 6)
+        {
+            Full.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Five.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Half.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            three.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Low.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            One.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        if (health == 5)
+        {
+            Full.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Five.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Half.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            three.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Low.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            One.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        if (health == 4)
+        {
+            Full.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Five.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Half.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            three.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Low.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            One.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+        }
 
         if (health == 3)
         {
-            Full.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-            Half.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-            Low.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Full.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Five.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Half.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            three.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Low.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            One.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
             return;
         }
         else if (health == 2)
         {
-            Full.transform.localScale = new Vector3(0, 0, 0);
-            Half.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Full.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Five.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Half.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            three.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
             Low.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            One.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
             return;
         }
         else if (health == 1) 
         {
-            Full.transform.localScale = new Vector3(0, 0, 0);
-            Half.transform.localScale = new Vector3(0, 0, 0);
-            Low.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+            Full.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Five.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Half.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            three.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            Low.transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+            One.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
             return;
         }
         return;
